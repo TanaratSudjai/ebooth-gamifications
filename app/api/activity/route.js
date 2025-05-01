ebc37d951d92d1ce1305744c6faacebb17de4b6d
ZonixEz <pnem2003@gmail.com>
2025-05-01 14:14:26 +0700

    activity secure

--- app/api/activity/route.js
+++ app/api/activity/route.js
@@ -1,9 +1,23 @@
 import { getPaginatedData } from "@/services/server/paginate";
 import { createActivity,getActivityData } from "@/services/server/activityService";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Ensure correct path
 const activity = "activity";
 
 export async function GET(req) {
+  const session = await getServerSession({ req, ...authOptions });
+
+  if (!session) {
+    return NextResponse.json(
+      {
+        status: "unauthorized",
+        message: "You must be logged in to access this resource.",
+      },
+      { status: 401 }
+    );
+  }
+
   try {
     const { searchParams } = new URL(req.url);
     const page = parseInt(searchParams.get("page")) || 1;
