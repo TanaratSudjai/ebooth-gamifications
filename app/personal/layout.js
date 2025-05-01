5e403d93953b4503997a4dea1c5fa26975f2d8b3
TanaratSudjai <650112418012@bru.ac.th>
2025-05-02 01:04:45 +0700

    personal start

---
+++ app/personal/layout.js
@@ -0,0 +1,15 @@
+// //layout for admin pages
+// import React from "react";
+
+
+// import DashboardTemplate from "./components/DashboardTemplate";
+// import { AlertProvider } from "@/contexts/AlertContext";
+// export default function AdminLayout({ children }) {
+//   return (
+//     <div>
+//       <AlertProvider>
+//         <DashboardTemplate children={children}></DashboardTemplate>
+//       </AlertProvider>
+//     </div>
+//   );
+// }
