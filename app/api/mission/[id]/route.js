40b6cbe730ec0d7b216825c1f034ce106f080eb4
ZonixEz <pnem2003@gmail.com>
2025-05-13 14:51:58 +0700

    fix json

--- app/api/mission/[id]/route.js
+++ app/api/mission/[id]/route.js
@@ -12,7 +12,7 @@ export async function PUT(req, {params}) {
     return NextResponse.json({ status: "error", message: result.error }, { status: result.status || 400 });
   }
 
-  return NextResponse.json({ status: "success", message: "Mission updated successfully", data: result });
+  return NextResponse.json({ status: "success", message: "Mission updated successfully", result });
 }
 
 // DELETE by ID
@@ -30,10 +30,6 @@ export async function DELETE(req, {params}) {
 
 export async function GET(req, {params}) {
   const id = Number(params.id);
-  const { searchParams } = new URL(req.url);
-  const page = searchParams.get("page") || 1;
-  const limit = searchParams.get("limit") || 10;
-
-  const result = await getMissionsById(id, page, limit);
-  return NextResponse.json({ status: "success", data: result });
+  const result = await getMissionsById(id);
+  return NextResponse.json(result);
 }
