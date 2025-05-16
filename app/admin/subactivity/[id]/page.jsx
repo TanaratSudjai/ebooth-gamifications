522329fafc3b5c75dcdd6753038728839a1972bc
EscThod <tanarat.dev18@gmail.com>
2025-05-16 13:18:03 +0700

    deployed

--- app/admin/subactivity/[id]/page.jsx
+++ app/admin/subactivity/[id]/page.jsx
@@ -25,6 +25,8 @@ function page() {
   const [selectedId, setSelectedId] = useState(null);
   const [selectedIdSub, setSelectedIdSub] = useState(null);
   const [loading, setLoading] = useState(true);
+  let date_start = "";
+  let date_end = "";
   const [formData, setFormData] = useState({
     sub_activity_name: "",
     sub_activity_description: "",
@@ -52,6 +54,8 @@ function page() {
       const res_sub = await axios.get(
         `/api/subActivity/getSubByActivity/${id}`
       );
+      // console.log("sub data : ", res_sub.data.data[0].sub_activity_start);
+      // console.log("sub data : ", res_sub.data.data[0].sub_activity_end);
 
       setSubActivity(res_sub.data.data);
       setLoading(false);
@@ -192,9 +196,17 @@ function page() {
                     </div>
                     <p className="p-1">{sub.sub_activity_description}</p>
                     <div className="flex flex-col md:flex-row gap-2 break-all">
-                      {formatDisplayDateTime(sub.sub_activity_start)}
-                      <p className="text-red-500"> จนถึง</p>
-                      {formatDisplayDateTime(sub.sub_activity_end)}
+                      <div className="flex flex-col md:flex-row gap-2 break-all">
+                        <span>
+                          วันที่ {sub.sub_activity_start.split("T")[0]} เวลา{" "}
+                          {sub.sub_activity_start.split("T")[1].slice(0, 5)}
+                        </span>
+                        <p className="text-red-500">จนถึง</p>
+                        <span>
+                          วันที่ {sub.sub_activity_end.split("T")[0]} เวลา{" "}
+                          {sub.sub_activity_end.split("T")[1].slice(0, 5)}
+                        </span>
+                      </div>
                     </div>
                     <div className="flex gap-5">
                       <p>จำนวนที่รับ {sub.sub_activity_max}</p>
