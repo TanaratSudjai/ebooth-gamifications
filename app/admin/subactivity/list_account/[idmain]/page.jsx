ad6fb8141d6475112eeb0029425e665d893aedf6
EscThod <tanarat.dev18@gmail.com>
2025-05-18 18:01:03 +0700

    table member form activity cart

--- app/admin/subactivity/list_account/[idmain]/page.jsx
+++ app/admin/subactivity/list_account/[idmain]/page.jsx
@@ -2,26 +2,41 @@
 import React, { use, useEffect, useState } from "react";
 import { useRouter, useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
+import CommonTextHeaderView from "@/app/admin/components/Common/TextHeader/View";
+import ListMember from "@/app/admin/components/Common/Table/ListMember";
+import { set } from "zod";
 function page() {
   const r = useRouter();
   const p = useParams();
   const searchParams = useSearchParams();
   const [member, setMember] = useState([]);
+  const [loading, setLoading] = useState(true);
   //   id activity
   const id_act = p?.idmain;
   const bool = searchParams.get("bool");
+  const name = searchParams.get("name");
+  console.log(name);
 
   const getMemberData = async () => {
     let response;
-    if (bool) {
-      response = await axios.get(`/api/getMemberByActivityId/${id_act}`);
+    if (bool === "true") {
+      response = await axios.get(
+        `/api/personnel/getMemberByActivityId/${id_act}`
+      );
+      console.log(response.data.data.members);
+
       if (response.status === 200) {
-        setMember(response.data);
+        setMember(response.data.data.members);
+        setLoading(false);
       }
     } else {
-      response = await axios.get(`/api/getMemberBySubActivityId/${id_act}`);
+      response = await axios.get(
+        `/api/personnel/getMemberBySubActivityId/${id_act}`
+      );
+      console.log(response.data.data.members);
       if (response.status === 200) {
-        setMember(response.data);
+        setMember(response.data.data.members);
+        setLoading(false);
       }
     }
   };
@@ -32,17 +47,10 @@ function page() {
 
   return (
     <div>
-      {bool === "true" ? (
-        <div>
-          <h1>List Account Main</h1>
-          {"ID Main: " + id_act}
-        </div>
-      ) : (
-        <div>
-          <h1>List Account Sub</h1>
-          {"ID Main: " + id_act}
-        </div>
-      )}
+      <CommonTextHeaderView text={`รายชื่อผู้เข้าร่วมกิจกรรม ${name}`} />
+      <div className="">
+        <ListMember data={member} loading={loading} />
+      </div>
     </div>
   );
 }
