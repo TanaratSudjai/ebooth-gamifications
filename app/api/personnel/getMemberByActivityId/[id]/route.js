8296d9c3d47e3f3e0aff641b4f3c358ab0707974
ZonixEz <pnem2003@gmail.com>
2025-05-18 14:25:16 +0700

    paginat of getmemberbyactivity

--- app/api/personnel/getMemberByActivityId/[id]/route.js
+++ app/api/personnel/getMemberByActivityId/[id]/route.js
@@ -1,10 +1,16 @@
 import { getMemberByActivityId } from "@/services/server/personelService";
 import { NextResponse } from "next/server";
 
+
 export async function GET(request, { params }) {
   try {
     const { id } = params;
-    const personnel = await getMemberByActivityId(id);
+    const { searchParams } = new URL(request.url);
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "10");
+
+    const personnel = await getMemberByActivityId(id, { page, limit });
+
     return NextResponse.json(personnel, { status: 200 });
   } catch (error) {
     console.error("❌ getMemberByActivityId error:", error);
@@ -14,3 +20,4 @@ export async function GET(request, { params }) {
     );
   }
 }
+
