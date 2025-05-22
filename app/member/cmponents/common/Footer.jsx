3d6c1b7d7c318f144cb7cf586fb03e30d3094b8a
EscThod <tanarat.dev18@gmail.com>
2025-05-22 17:00:23 +0700

    page route

--- app/member/cmponents/common/Footer.jsx
+++ app/member/cmponents/common/Footer.jsx
@@ -1,22 +1,48 @@
+"use client";
 import React from "react";
-
+import { useRouter } from "next/navigation";
 function Footer() {
+  const r = useRouter();
+
+  const handleClick = (path) => {
+    r.push("/member/" + path);
+  };
   return (
     <footer className="bg-gray-800 text-white">
-      {/* <div className="flex flex-wrap justify-around items-center p-4 gap-4">
-        <button className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded text-sm transition">
-          HOME
-        </button>
-        <button className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded text-sm transition">
-          ABOUT
-        </button>
-        <button className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded text-sm transition">
-          SERVICES
-        </button>
-        <button className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded text-sm transition">
-          CONTACT
-        </button>
-      </div> */}
+      <div className="flex justify-around gap-2 p-2">
+        <div
+          onClick={() => {
+            handleClick("activity_member");
+          }}
+          className="py-1 px-3 bg-white shadow rounded-md cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out text-gray-800"
+        >
+          <span>กิจกรรม</span>
+        </div>
+        <div
+          onClick={() => {
+            handleClick("profile_member");
+          }}
+          className="py-1 px-3 bg-white shadow rounded-md cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out text-gray-800"
+        >
+          <span>โปรไฟล์</span>
+        </div>
+        <div
+          onClick={() => {
+            handleClick("");
+          }}
+          className="py-1 px-3 bg-white shadow rounded-md cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out text-gray-800"
+        >
+          <span>SCAN</span>
+        </div>
+        <div
+          onClick={() => {
+            handleClick("history_member");
+          }}
+          className="py-1 px-3 bg-white shadow rounded-md cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out text-gray-800"
+        >
+          <span>ประวัติการเข้าร่วม</span>
+        </div>
+      </div>
       <div className="text-center text-xs py-2 border-t border-gray-600">
         &copy; 2025 Webb Application eBooth All rights reserved.
       </div>
